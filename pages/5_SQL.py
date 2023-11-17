56b8decdd163872a6c53ad63b566b4b682289669
Mathias Halkjær <94175492+MathiasHalkjaer@users.noreply.github.com>
2023-11-17 23:48:42 +0000

    update sql

--- pages/5_SQL.py
+++ pages/5_SQL.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 # Streamlit app
 def main():
-    conn = st.experimental_connection("sql")
+    conn = st.connection("sql")
+    pet_owners = conn.query('select top 10 * from Sales.Customer')
+    st.dataframe(pet_owners)
 if __name__ == "__main__":
     main()
