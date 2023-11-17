78ceebcbb6242607bd4933bbf0a694bde3cc7094
Mathias Halkjær <94175492+MathiasHalkjaer@users.noreply.github.com>
2023-11-17 23:54:33 +0000

    teoaete okera

--- pages/5_SQL.py
+++ pages/5_SQL.py
@@ -5,6 +5,8 @@ import pandas as pd
 def main():
     conn = st.connection("sql")
     pet_owners = conn.query('select top 10 * from Sales.Customer')
+    st.write("Test")
     st.dataframe(pet_owners)
+    st.write(pet_owners)
 if __name__ == "__main__":
     main()
